8dd23818167ace6c60ebcda23e3693a1bce1538e
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2019-03-14 21:51:53 +0000

    Add basic support for plugins.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ export const createApp = ({
 } = {}) => {
   for (const [className, component] of Object.entries(components)) {
     const CustomElement = createComponent(className, component);
+    for (const plugin of plugins) plugin(CustomElement);
     customElements.define(CustomElement.tagName, CustomElement);
   }
 };
